720c01880782aa690b399fa3574f24b898ce8930
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-19 15:22:23 +0530

    use ref in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-function App()
-{
-  return(
+import { useRef } from "react";
+
+function App() {
+  const inputref = useRef(null);
+
+  const inputhandle = () => {
+    inputref.current.focus();
+  };
+
+  return (
     <>
-    <h1>hello word</h1>
+      <h1>useref in react js</h1>
+      <input type="text" placeholder="Enter your name" ref={inputref} />
+      <button onClick={inputhandle}>focus</button>
     </>
-  )
+  );
 }
 
 export default App;
