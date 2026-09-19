c12c344f509478114861d131d2993617efa80774
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-19 15:56:28 +0530

    uncontrolled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { useRef } from "react";
-
 function App() {
-  const inputref = useRef(null);
 
-  const inputhandle = () => {
-    inputref.current.focus();
+  const handleForm = (event) => {
+    event.preventDefault();
+
+    const user = document.querySelector("#name").value;
+    const pass = document.querySelector("#pass").value;
+    console.log(user,pass);
   };
 
   return (
     <>
-      <h1>useref in react js</h1>
-      <input type="text" placeholder="Enter your name" ref={inputref} />
-      <button onClick={inputhandle}>focus</button>
+      <h1>uncontrolled component</h1>
+      <form onSubmit={handleForm}>
+        <input type="text" placeholder="enter name" id="name" />
+        <br />
+        <input type="password" placeholder="enter password" id="pass" />
+        <br />
+        <button>submit</button>
+      </form>
     </>
   );
 }
