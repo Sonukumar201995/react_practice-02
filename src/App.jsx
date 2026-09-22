9a07f6bce711cd103255e01fd2f5304c62b8afc4
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-22 14:24:11 +0530

    update object in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,79 @@
+
 import { useState } from "react";
-import Display from "./Display";
-import User from "./User";
 
-function App()
-{
+function App() {
+  const [data, setData] = useState({
+    name: "sonu",
+    address: {
+      city: "New Delhi",
+      country: "India",
+    },
+  });
 
-    const[user,setUser]=useState('');
-    return(
-        <>
-            <User setUser={setUser}/>
-            <Display user={user}/>
-        </>
-    )
+  // Update Name
+  const handleName = (val) => {
+    setData({
+      ...data,
+      name: val,
+    });
+  };
+
+  // Update City
+  const handleCity = (val) => {
+    setData({
+      ...data,
+      address: {
+        ...data.address,
+        city: val,
+      },
+    });
+  };
+
+  // Update Country
+  const handleCountry = (val) => {
+    setData({
+      ...data,
+      address: {
+        ...data.address,
+        country: val,
+      },
+    });
+  };
+
+  return (
+    <>
+      <h2>Update Object in State</h2>
+
+      <input
+        type="text"
+        placeholder="Update Name"
+        onChange={(e) => handleName(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="text"
+        placeholder="Update City"
+        onChange={(e) => handleCity(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="text"
+        placeholder="Update Country"
+        onChange={(e) => handleCountry(e.target.value)}
+      />
+
+      <h3>Name: {data.name}</h3>
+      <h3>City: {data.address.city}</h3>
+      <h3>Country: {data.address.country}</h3>
+    </>
+  );
 }
 
 export default App;
+
