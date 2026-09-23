da792b9bfa8453099c9486fd144f9b94e71f505f
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-23 11:29:23 +0530

    useId in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,14 @@
-import { useActionState } from "react";
+import { useId } from "react";
 
 function App() {
+  const id = useId();
 
-    const handleSubmit = (preData, formData) => {
-
-        let name = formData.get("name");
-        let password = formData.get("password");
-
-        if (name && password) {
-            return {
-                message: "Data submitted"
-            };
-        } else {
-            return {
-                error: "Data not submitted. Enter correct name & password"
-            };
-        }
-    };
-
-    const [data, action, pending] = useActionState(
-        handleSubmit,
-        undefined
-    );
-
-    console.log(data);
-
-    return (
-        <>
-            <h2>useActionState in React</h2>
-
-            <form action={action}>
-
-                <input
-                    type="text"
-                    placeholder="Enter name"
-                    name="name"
-                />
-
-                <br />
-                <br />
-
-                <input
-                    type="password"
-                    placeholder="Enter password"
-                    name="password"
-                />
-
-                <br />
-                <br />
-
-                <button disabled={pending}>
-                    {pending ? "Submitting..." : "Submit"}
-                </button>
-                    <br />
-                {data?.error && ( <span>{data.error}</span>)}
-
-                {data?.message && (<span>{data.message}</span>)}
-
-            </form>
-        </>
-    );
+  return (
+    <>
+      <label htmlFor={id}>Username</label>
+      <input id={id} type="text" />
+    </>
+  );
 }
 
 export default App;
