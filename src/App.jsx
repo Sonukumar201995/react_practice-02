337ffb606b08522b07a2691f2742396b3714a81f
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-23 11:22:27 +0530

    useActionState in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from "react";
+import { useActionState } from "react";
 
 function App() {
-    const [data, setData] = useState([
-        "sonu",
-        "arun",
-        "monu",
-        "sandip",
-        "shekhar"
-    ]);
 
-    const updateData = (name) => {
-        const newData = [...data];
-        newData[newData.length - 1] = name;
-        setData(newData);
+    const handleSubmit = (preData, formData) => {
+
+        let name = formData.get("name");
+        let password = formData.get("password");
+
+        if (name && password) {
+            return {
+                message: "Data submitted"
+            };
+        } else {
+            return {
+                error: "Data not submitted. Enter correct name & password"
+            };
+        }
     };
+
+    const [data, action, pending] = useActionState(
+        handleSubmit,
+        undefined
+    );
+
+    console.log(data);
 
     return (
         <>
-            <h2>Update Array in State</h2>
+            <h2>useActionState in React</h2>
 
-            <input
-                type="text"
-                placeholder="Update name"
-                onChange={(e) => updateData(e.target.value)}
-            />
+            <form action={action}>
 
-            {
-                data.map((item, index) => {
-                    return <h2 key={index}>{item}</h2>;
-                })
-            }
+                <input
+                    type="text"
+                    placeholder="Enter name"
+                    name="name"
+                />
+
+                <br />
+                <br />
+
+                <input
+                    type="password"
+                    placeholder="Enter password"
+                    name="password"
+                />
+
+                <br />
+                <br />
+
+                <button disabled={pending}>
+                    {pending ? "Submitting..." : "Submit"}
+                </button>
+                    <br />
+                {data?.error && ( <span>{data.error}</span>)}
+
+                {data?.message && (<span>{data.message}</span>)}
+
+            </form>
         </>
     );
 }
