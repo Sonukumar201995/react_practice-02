cc83e6ddb72c4a5649e602611a1efb9fbfabf6b8
Sonu Kumar <140957411+Sonukumar201995@users.noreply.github.com>
2026-09-24 11:34:09 +0530

    custom hook in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import useToggle from "./useToggle";
 
-function App() {
-  return (
-    <React.Fragment>
-      <h1>Hello</h1>
-      <p>Welcome</p>
-    </React.Fragment>
-  );
+function App()
+{
+  const [value,setValue]=useToggle(true)
+  return(
+    <>
+      
+      <button onClick={()=>setValue()}>toggle</button>
+      <button onClick={()=>setValue(false)}>hide</button>
+      <button onClick={()=>setValue(true)}>show</button>
+      {
+        value ?<h2>custom hook </h2>:null
+      }
+    </>
+  )
 }
+
+export default App;
